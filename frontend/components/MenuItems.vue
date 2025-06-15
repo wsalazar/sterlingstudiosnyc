7ce7f49147980eda847f9bda990825d68f750964
Will Salazar <will.a.salazar@gmail.com>
2025-06-15 07:50:38 -0400

    Added gallery page. Added link to gallery page. Changed some things in the default layout to differentiate admin from a regular user

--- frontend/components/MenuItems.vue
+++ frontend/components/MenuItems.vue
@@ -1,6 +1,24 @@
 <template>
-  <div>MenuItems</div>
+  <nav class="m-4">
+    <NuxtLink
+      to="/gallery"
+      class="block px-4 py-2 text-amber-500 hover:bg-black rounded-md transition-colors duration-200"
+      :class="{ 'bg-black': route.path === '/gallery' }"
+    >
+      Gallery
+    </NuxtLink>
+  </nav>
 </template>
-<script setup lang="ts"></script>
 
-<style scoped></style>
+<script setup lang="ts">
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+</script>
+
+<style scoped>
+.router-link-active {
+  background-color: black;
+  color: rgb(245 158 11);
+}
+</style>
