c46aad056cac3220d7339aa48588ca675d1b20a6
Will Salazar <will.a.salazar@gmail.com>
2025-07-10 12:55:13 -0400

    Added notification system and a processing spinner

---
+++ frontend/components/Spinner.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="spinner"></div>
+</template>
+
+<style scoped>
+.spinner {
+  border: 4px solid #000000;
+  border-top: 4px solid #f59e0b;
+  border-radius: 50%;
+  width: 100px;
+  height: 100px;
+  animation: spin 1s linear infinite;
+  margin: auto;
+}
+@keyframes spin {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+</style>
